8079407bf6b397668517cd8b4f5db7c1ce7b529c
Dry-zaN <AEBLEMOST@protonmail.ch>
2023-01-24 10:55:37 +0100

    Deleted variables in order to simplify code.

--- ipv4Tool_FS/Program.cs
+++ ipv4Tool_FS/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
@@ -10,16 +11,7 @@ internal class Program
     {
         DisplayIPAddresses();
         Console.ReadKey();
-    }
-
-    public static void Menu(string[] args)
-    {
-        Console.Clear();
-        Console.WriteLine("1. Set a new IP address");
-        Console.WriteLine("2. Unrestrict access to censored websites (i.e. thepiratebay.org");
-        Console.WriteLine("3. Connect to your router");
-        Console.WriteLine("4. FAQ");
-        Console.ReadKey();
+        Menu();
     }
 
     public static string DisplayIPAddresses()
@@ -54,33 +46,21 @@ internal class Program
                     
                     string vpnCheck = network.Name.ToString();
 
-                    int vpnDeactivate = 0;
-                    int vpnActivated = 0;
+                    Console.WriteLine(vpnCheck);
+                    Console.ReadKey();
+
+
 
                     //if test contain
-                    if (vpnCheck.Contains("utun"))
+                    if (vpnCheck.Contains("en"))
                     {
-                        vpnDeactivate = 1;
-                        vpnActivated = 5;
-                        vpnCheck = vpnCheck.Replace("utun3", "a VPN").ToString();
-                    }
-                    else if (vpnCheck.Contains("ipsec"))
-                    {
-                        vpnDeactivate = 1;
-                        vpnActivated = 5;
-                        vpnCheck = vpnCheck.Replace("ipsec0", "a VPN").ToString();
-                    }
-                    else if (vpnCheck.Contains("en"))
-                    {
-                        vpnDeactivate = 2;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("PROBLEM");
-                    }
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(vpnCheck);
 
-                    if (vpnDeactivate == 1 && vpnActivated == 5)
+                        Console.WriteLine("Your Local IP address is: {0}\nYou are using a wireless interface to achieve this connection called: {1}.\n", address.Address, vpnCheck, network.Description);
+                        Console.ReadKey();
+                    }
+                    else if (vpnCheck.Contains("ipsec") || vpnCheck.Contains("mu"))
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("This application does not support the use of a Virtual Private Network (VPN)\nPlease disable your VPN and come back once it has been disabled.");
@@ -89,22 +69,7 @@ internal class Program
                         Console.ReadKey();
                         Environment.Exit(0);
                     }
-                    else if (vpnDeactivate == 2 && vpnActivated != 5)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine(vpnCheck + " " + vpnDeactivate);
-
-                        Console.WriteLine("Your Local IP address is: {0}\nYou are using a wireless interface to achieve this connection called: {1}.\n", address.Address, vpnCheck, network.Description);
-                        Console.ReadKey();
-                        Console.Clear();
-                        Console.WriteLine("1. Set a new IP address");
-                        Console.WriteLine("2. Unrestrict access to censored websites (i.e. thepiratebay.org");
-                        Console.WriteLine("3. Connect to your router");
-                        Console.WriteLine("4. FAQ");
-                        Console.ReadKey();
-
-                    }
-                    else if (vpnDeactivate == 0)
+                    else 
                     {
                         Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine("You shouldn't have ended up here.");
@@ -119,6 +84,18 @@ internal class Program
             }
         }
         return returnAddress;
+
+
     }
-                    
+
+    private static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("1. Set a new IP address");
+        Console.WriteLine("2. Unrestrict access to censored websites (i.e. thepiratebay.org");
+        Console.WriteLine("3. Connect to your router");
+        Console.WriteLine("4. FAQ");
+        Console.ReadKey();
+    }
+
 }
